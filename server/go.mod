18e926663b00a1d5f3a69963f2e6676f6b3b5abe
Tiago Cardoso <tiago.nascimento@dasa.com.br>
2024-08-26 21:48:22 -0300

    feat: add sqlite context persistence

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module github.com/tiagoncardoso/fc/pge/client-server-api/server
 
 go 1.23.0
+
+require github.com/mattn/go-sqlite3 v1.14.22
--- server/main.go
+++ server/main.go
@@ -2,24 +2,25 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"encoding/json"
-	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
 	"time"
 )
 
 type TimeOut int
-type ApiUrl string
 
 // TODO: Change RequestTimeOut to 200 before send to FC
 const (
-	RequestTimeOut             TimeOut = 300
+	RequestTimeOut             TimeOut = 200
 	DatabasePersistenceTimeOut         = 10
 )
 
 const (
-	ExchangeApiUrl ApiUrl = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+	ExchangeApiUrl string = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+	HttpPort              = ":8080"
 )
 
 type ExchangeData struct {
@@ -40,16 +41,65 @@ type ExchangeApiResponse struct {
 	USDBRL ExchangeData `json:"USDBRL"`
 }
 
+type ExchangeOutput struct {
+	BID string `json:"bid"`
+}
+
 func main() {
-	apiResp, err := requestExchange()
-	if err != nil {
-		log.Fatal("Error on API request", err)
+	if err := startWebServer(); err != nil {
+		log.Fatal("Error on starting server", err)
+	}
+}
+
+func startWebServer() error {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		//http.Redirect(w, r, "/cotacao", http.StatusSeeOther)
+		w.Write([]byte("Hello from server"))
+	})
+	mux.HandleFunc("/cotacao", func(w http.ResponseWriter, r *http.Request) {
+		apiResp, err := requestExchange()
+		if err != nil {
+			log.Println("[ERROR]:: Error on requesting data")
+			panic(err)
+		}
+
+		err = saveExchangeData(apiResp)
+		if err != nil {
+			log.Println("[ERROR]:: Error on saving data")
+			panic(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		jsonResp, err := json.Marshal(ExchangeOutput{BID: apiResp.Bid})
+		if err != nil {
+			log.Println("[ERROR]:: Error on marshalling response")
+			panic(err)
+		}
+
+		w.Write(jsonResp)
+	})
+
+	log.Println("Starting server on", HttpPort)
+	if err := http.ListenAndServe(HttpPort, recoveryMiddleware(mux)); err != nil {
+		return err
 	}
 
-	err = saveExchangeData(apiResp)
-	if err != nil {
-		log.Fatal("Error on saving data", err)
-	}
+	return nil
+}
+
+func recoveryMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("Recover from panic: %v\n", err)
+				//debug.PrintStack()
+				http.Error(w, "Internal server error", http.StatusInternalServerError)
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 func requestExchange() (ExchangeData, error) {
@@ -58,7 +108,7 @@ func requestExchange() (ExchangeData, error) {
 	defer cancel()
 
 	cl := http.Client{}
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, string(ExchangeApiUrl), nil)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, ExchangeApiUrl, nil)
 	if err != nil {
 		return ExchangeData{}, err
 	}
@@ -82,9 +132,23 @@ func saveExchangeData(data ExchangeData) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*time.Duration(DatabasePersistenceTimeOut))
 	defer cancel()
 
-	fmt.Println(ctx)
+	db, err := sql.Open("sqlite3", "./awesomeapi.db")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
 
-	fmt.Println(data)
+	stmt, err := db.Prepare("INSERT INTO results(code, codein, name, high, low, varBid, pctChange, bid, ask, timestamp, create_date) " +
+		"VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ctx, data.Code, data.Codein, data.Name, data.High, data.Low, data.VarBid, data.PctChange, data.Bid, data.Ask, data.Timestamp, data.CreateDate)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
